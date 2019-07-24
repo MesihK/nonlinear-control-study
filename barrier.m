3e3be1df85701295b04581f58ee7257cc4f74930
Mesih Kilinc <mesihkilinc@gmail.com>
2019-07-24 11:40:41 +0300

    add barrier lypunov funtion plotting

---
+++ barrier.m
@@ -0,0 +1,26 @@
+% author: Mesih Veysi Kılınç
+% date:   24-07-2019
+% title:  Plot barrier lypunov funtions
+ln = [];
+trig = [];
+trig2 = [];
+index = 1;
+
+length = 0.999;
+step = 0.001;
+for t=-length:step:length
+  ln(index)   = log(1/(1-t*t));
+  trig(index) = tan(pi/2*t*t);
+  trig2(index) = tan(pi/2*t)^2;
+  
+  if(trig(index) > 10) trig(index) = 10; endif;
+  if(trig2(index) > 10) trig2(index) = 10; endif;
+  if(ln(index) > 10) ln(index) = 10; endif;
+  index += 1;
+endfor
+
+hold on;
+plot(-length:step:length, ln);
+plot(-length:step:length, trig);
+plot(-length:step:length, trig2);
+legend("ln","tan", "tan^2");
